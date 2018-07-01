0d57b1144b13b9e9d915c26dd37d4dca5cbad51d
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2018-07-01 08:59:06 +0530

    back added

--- js/main.js
+++ js/main.js
@@ -6,11 +6,18 @@ sitemap:
 
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    if ($('.panel-cover').hasClass('panel-cover--collapsed'))
+    {
+      $('.panel-cover').css('max-width', 'none')
+      $('.panel-cover').css('width', '100%')
+     $('.panel-cover').css('background-position', 'center')
+    }
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
+     $('.panel-cover').css('max-width', 'none')
+      $('.panel-cover').css('width', '100%')
+     $('.panel-cover').css('background-position', 'center')
+     $('.panel-cover').addClass('animated slideInLeft')
     } else {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
